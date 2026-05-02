eba3313e1c5b59efae32e94bed4188e8cafa1271
Chandan Yadav <chandanyadavofficial06@gmail.com>
2026-05-02 23:11:22 +0530

    Built PDF Merger

---
+++ PDF Merger/main.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const multer = require("multer");
+const {mergepdfs} = require("./merge");
+const upload = multer({ dest: "uploads/" });
+const app = express();
+app.use('/static', express.static('public'));
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "templates/index.html"));
+});
+
+app.post("/merge", upload.array("pdfs", 12), async function (req, res, next) {
+  console.log(req.files);
+  await mergepdfs(
+    path.join(__dirname, req.files[0].path),
+    path.join(__dirname, req.files[1].path),
+  );
+  res.redirect(`http://localhost:3000/static/${d}.pdf`);
+  // res.send({data: req.files})
+  // req.files is array of `photos` files
+  // req.body will contain the text fields, if there were any
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
